e33d61884c160d6b213b7cf45f685c4a47e63114
LambdaGaming <starwars18m@gmail.com>
2023-09-02 12:27:31 -0400

    Update to exiled 8

--- RandomGamemode/Plugin.cs
+++ RandomGamemode/Plugin.cs
@@ -9,8 +9,8 @@ namespace RandomGamemode
 	public class Plugin : Plugin<Config>
 	{
 		private EventHandlers EventHandlers;
-		public override Version Version { get; } = new Version( 1, 6, 1 );
-		public override Version RequiredExiledVersion { get; } = new Version( 7, 0, 0 );
+		public override Version Version { get; } = new Version( 1, 6, 2 );
+		public override Version RequiredExiledVersion { get; } = new Version( 8, 0, 0 );
 		public override PluginPriority Priority { get; } = PluginPriority.Medium;
 		public static List<int> EnabledList = new List<int>();
 
