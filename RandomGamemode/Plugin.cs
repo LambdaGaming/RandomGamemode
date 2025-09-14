94238398264b03d3190d2e55a41e6dd68d9d80ed
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2025-09-14 15:22:48 -0400

    Version bump

--- RandomGamemode/Plugin.cs
+++ RandomGamemode/Plugin.cs
@@ -9,8 +9,8 @@ namespace RandomGamemode
 	public class Plugin : Plugin<Config>
 	{
 		private EventHandlers EventHandlers;
-		public override Version Version { get; } = new Version( 2, 7, 0 );
-		public override Version RequiredExiledVersion { get; } = new Version( 9, 4, 0 );
+		public override Version Version { get; } = new Version( 2, 7, 1 );
+		public override Version RequiredExiledVersion { get; } = new Version( 9, 8, 0 );
 		public override string Author { get; } = "OPGman";
 		public override PluginPriority Priority { get; } = PluginPriority.Medium;
 
